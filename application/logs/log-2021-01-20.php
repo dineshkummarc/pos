cab268a3b373aa00f46cd67c35cdb740c29e744c
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-01-20 22:05:44 +0530

    update

---
+++ application/logs/log-2021-01-20.php
@@ -0,0 +1,34 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2021-01-20 17:01:50 --> Severity: Notice  --> Only variable references should be returned by reference C:\xampp\htdocs\test\pos\system\core\Common.php 257
+ERROR - 2021-01-20 17:01:51 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; MY_Lang has a deprecated constructor C:\xampp\htdocs\test\pos\application\core\MY_Lang.php 2
+ERROR - 2021-01-20 17:01:51 --> Language file contains no data: language/english/items_lang.php
+ERROR - 2021-01-20 17:01:51 --> Language file contains no data: language/english/sales_lang.php
+ERROR - 2021-01-20 17:01:51 --> Language file contains no data: language/english/suppliers_lang.php
+ERROR - 2021-01-20 17:01:51 --> Language file contains no data: language/english/giftcards_lang.php
+ERROR - 2021-01-20 17:01:51 --> Language file contains no data: language/english/item_kits_lang.php
+ERROR - 2021-01-20 17:01:52 --> Severity: Warning  --> Declaration of Customer::save(&$person_data, &$customer_data, $customer_id = false) should be compatible with Person::save(&$person_data, $person_id = false) C:\xampp\htdocs\test\pos\application\models\customer.php 448
+ERROR - 2021-01-20 17:01:52 --> Severity: Warning  --> Declaration of Employee::save(&$person_data, &$employee_data, &$permission_data, &$permission_action_data, &$location_data, $employee_id = false) should be compatible with Person::save(&$person_data, $person_id = false) C:\xampp\htdocs\test\pos\application\models\employee.php 576
+ERROR - 2021-01-20 17:01:52 --> Severity: Warning  --> Declaration of Supplier::save(&$person_data, &$supplier_data, $supplier_id = false) should be compatible with Person::save(&$person_data, $person_id = false) C:\xampp\htdocs\test\pos\application\models\supplier.php 398
+ERROR - 2021-01-20 17:01:52 --> Query error: Table 'pos.app_config' doesn't exist
+ERROR - 2021-01-20 17:25:56 --> Severity: Notice  --> Only variable references should be returned by reference C:\xampp\htdocs\test\pos\system\core\Common.php 257
+ERROR - 2021-01-20 17:25:58 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; MY_Lang has a deprecated constructor C:\xampp\htdocs\test\pos\application\core\MY_Lang.php 2
+ERROR - 2021-01-20 17:25:58 --> Language file contains no data: language/english/items_lang.php
+ERROR - 2021-01-20 17:25:58 --> Language file contains no data: language/english/sales_lang.php
+ERROR - 2021-01-20 17:25:58 --> Language file contains no data: language/english/suppliers_lang.php
+ERROR - 2021-01-20 17:25:58 --> Language file contains no data: language/english/giftcards_lang.php
+ERROR - 2021-01-20 17:25:58 --> Language file contains no data: language/english/item_kits_lang.php
+ERROR - 2021-01-20 17:26:00 --> Severity: Warning  --> Declaration of Customer::save(&$person_data, &$customer_data, $customer_id = false) should be compatible with Person::save(&$person_data, $person_id = false) C:\xampp\htdocs\test\pos\application\models\customer.php 448
+ERROR - 2021-01-20 17:26:00 --> Severity: Warning  --> Declaration of Employee::save(&$person_data, &$employee_data, &$permission_data, &$permission_action_data, &$location_data, $employee_id = false) should be compatible with Person::save(&$person_data, $person_id = false) C:\xampp\htdocs\test\pos\application\models\employee.php 576
+ERROR - 2021-01-20 17:26:00 --> Severity: Warning  --> Declaration of Supplier::save(&$person_data, &$supplier_data, $supplier_id = false) should be compatible with Person::save(&$person_data, $person_id = false) C:\xampp\htdocs\test\pos\application\models\supplier.php 398
+ERROR - 2021-01-20 17:26:00 --> Query error: Table 'demo_db.app_config' doesn't exist
+ERROR - 2021-01-20 17:33:43 --> Severity: Notice  --> Only variable references should be returned by reference C:\xampp\htdocs\test\pos\system\core\Common.php 257
+ERROR - 2021-01-20 17:33:43 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; MY_Lang has a deprecated constructor C:\xampp\htdocs\test\pos\application\core\MY_Lang.php 2
+ERROR - 2021-01-20 17:33:43 --> Language file contains no data: language/english/items_lang.php
+ERROR - 2021-01-20 17:33:43 --> Language file contains no data: language/english/sales_lang.php
+ERROR - 2021-01-20 17:33:43 --> Language file contains no data: language/english/suppliers_lang.php
+ERROR - 2021-01-20 17:33:43 --> Language file contains no data: language/english/giftcards_lang.php
+ERROR - 2021-01-20 17:33:43 --> Language file contains no data: language/english/item_kits_lang.php
+ERROR - 2021-01-20 17:33:43 --> Severity: Warning  --> Declaration of Customer::save(&$person_data, &$customer_data, $customer_id = false) should be compatible with Person::save(&$person_data, $person_id = false) C:\xampp\htdocs\test\pos\application\models\customer.php 448
+ERROR - 2021-01-20 17:33:43 --> Severity: Warning  --> Declaration of Employee::save(&$person_data, &$employee_data, &$permission_data, &$permission_action_data, &$location_data, $employee_id = false) should be compatible with Person::save(&$person_data, $person_id = false) C:\xampp\htdocs\test\pos\application\models\employee.php 576
+ERROR - 2021-01-20 17:33:43 --> Severity: Warning  --> Declaration of Supplier::save(&$person_data, &$supplier_data, $supplier_id = false) should be compatible with Person::save(&$person_data, $person_id = false) C:\xampp\htdocs\test\pos\application\models\supplier.php 398
